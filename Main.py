b131dfe0bf923f00826a00e4aa9538906014f549
PolinaKrp <karpacevapolina@gmail.com>
2022-12-22 17:29:51 +0400

    Function takes data from 2 csv files and create dataframe with 2 columns
    Function takes data from 2 csv files and create dataframe with 2 columns

---
+++ Main.py
@@ -0,0 +1,22 @@
+import os
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import cv2
+
+pd.options.mode.chained_assignment = None
+
+def create_DataFrame() -> pd.DataFrame:
+    """This function takes data from 2 csv files and create dataframe with 2 columns"""
+    df1 = pd.read_csv(
+        os.path.join("/Users", "79276", "Desktop", "Lab4Sem3", "annotationleopard.csv"), 
+                      sep=',',
+                      header=None)
+    df2 = pd.read_csv(
+        os.path.join("/Users", "79276", "Desktop", "Lab4Sem3", "annotationtiger.csv"), sep=',',
+        header=None)
+    df = pd.concat([df1, df2], ignore_index=True)
+    df.drop(1, axis=1, inplace=True)
+    df.rename(columns={0: 'absolute_path', 2: 'dataset_class'}, inplace=True)
+    return df
